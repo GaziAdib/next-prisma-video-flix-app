d5bd18434c72415e4657c8cffbd8cea1ebda0792
unknown <adibcse10@gmail.com>
2023-05-02 17:37:42 +0600

    added server side and client side route protection

---
+++ pages/api/current.js
@@ -0,0 +1,18 @@
+import serverAuth from "@/lib/serverAuth";
+
+export default async function handler(req, res) {
+
+    if (req.method !== 'GET') {
+        return res.status(405).end();
+    }
+
+    try {
+        const { currentUser } = await serverAuth(req);
+        return res.status(200).json(currentUser);
+    } catch (error) {
+        console.log(error);
+        return res.status(400).end();
+    }
+
+
+}
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,37 @@
+import useCurrentUser from '@/hooks/useCurrentUser';
+import { getSession, signOut } from 'next-auth/react';
+
+export async function getServerSideProps(context) {
+
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+
+}
+
 export default function Home() {
+
+  const { data: user } = useCurrentUser();
+
   return (
 
     <>
-      <h1 className="text-2xl text-green-500">Netflix Clone</h1>
+      <h1 className="text-4xl text-green-500">Netflix Clone</h1>
+
+      <p className='text-white'>Logged In as : {user?.name}</p>
+
+      <button className='h-10 w-full bg-white' onClick={() => signOut()}>Logout</button>
     </>
 
   )
